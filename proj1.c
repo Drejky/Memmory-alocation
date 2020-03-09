658cc493cbda54a64aa5d9d1f92dbf303e746add
Drejky <peter.kudela9@gmail.com>
2020-03-09 23:17:16 +0100

    Adding memmory aloc function

--- proj1.c
+++ proj1.c
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
+//#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,6 +9,35 @@ char* start;
 void *memory_alloc(unsigned int size) {
 	int hSize = sizeof(unsigned short) + sizeof(char);
 	int fSize = sizeof(unsigned short);
+	unsigned short oldSize;
+
+	char* curr = start;
+	
+	while (1) {
+		if (*((char*)curr + sizeof(unsigned short)) == 0 && *(unsigned short*)curr > (hSize + fSize + size)) {
+			oldSize = *((unsigned short*)curr);
+			*(unsigned short*)curr = hSize + size + fSize;
+			*((char*)(curr + sizeof(unsigned short))) = 1;	//Rewriting header
+			*((unsigned short*)(curr + size + hSize)) = hSize + size + fSize;  //Creating new footer for allocated block
+			if (oldSize - hSize - fSize - size >= 5){			//If we have space for another free block left we give it a header and footer
+				*((unsigned short*)(curr + size + hSize + fSize)) = oldSize - hSize - fSize - size;
+				*((char*)(curr + size + hSize + fSize + sizeof(unsigned short))) = 0;
+				*((unsigned short*)(curr + oldSize - fSize)) = oldSize - hSize - fSize - size;	//Rewriting the original footer
+			}
+			return (curr + hSize);
+			
+		}
+		else if (*((char*)curr + sizeof(unsigned short)) == 0 && *(unsigned short*)curr == (hSize + fSize + size)) {	//If the free block fits perfectly we just mark it as allocated and return pointer
+			*((char*)(curr + sizeof(unsigned short))) = 1;
+			return (curr + hSize);
+		}
+		else {
+			curr += *((unsigned short*)curr);
+			continue;
+		}
+		return NULL;
+	}
+
 	return 0;
 }
 
@@ -21,23 +50,21 @@ int memory_check(void *ptr) {
 }
 
 void memory_init(void *ptr, unsigned int size) {
-	start = (char*)ptr; 
-	/*
-	*((int*)start) = (size - 2*sizeof(int)) * -1;
-	*((int*)(start + size - sizeof(int))) = (size - 2 * sizeof(int)) * -1;
-	*/
-	*((unsigned short*)start) = size - 2 * sizeof(unsigned short) - sizeof(char);
+	start = (char*)ptr;
+	*((unsigned short*)start) = size;
 	*((char*)start + sizeof(unsigned short)) = 0;
-	*((unsigned short*)(start + size - sizeof(unsigned short))) = size - 2 * sizeof(unsigned short) + sizeof(char);
-	printf("%d", *(start));
+	*((unsigned short*)(start + size - sizeof(unsigned short))) = size;
 
 }
 
 int main()
 {
-	char region[10];
-	memory_init(region, 10);/*
+	char region[30];
+	memory_init(region, 30);
 	char* pointer = (char*)memory_alloc(10);
+	char* pointer2 = (char*)memory_alloc(9);
+	pointer[0] = 'u';
+	/*
 	if (pointer)
 		memset(pointer, 0, 10);
 	if (pointer)
